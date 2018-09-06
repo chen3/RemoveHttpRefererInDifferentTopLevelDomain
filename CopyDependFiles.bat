02eab27a22aa374eb0e93b83c2cae5984dbb6243
chen3 <laoc1995@163.com>
2018-09-06 17:54:26 +0800

    移除当前项目使用parcel编译所需脚本
    更改为使用webpack后已经不需要该脚本

--- CopyDependFiles.bat
+++
@@ -1,6 +0,0 @@
-mkdir .\dist
-copy /Y .\manifest.json .\dist\manifest.json
-copy /Y .\node_modules\typescript-collections\dist\lib\umd.min.js .\dist\typescript-collections.min.js
-copy /Y .\node_modules\ts-events\dist\ts-events.min.js .\dist\ts-events.min.js
-copy /Y .\node_modules\vue\dist\vue.min.js .\dist\vue.min.js
-copy /Y .\node_modules\vuetify\dist\vuetify.min.js .\dist\vuetify.min.js
--- CopyDependFiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir ./dist
-cp -f ./manifest.json ./dist/manifest.json
-cp -f ./node_modules/typescript-collections/dist/lib/umd.min.js ./dist/typescript-collections.min.js
-cp -f ./node_modules/ts-events/dist/ts-events.min.js ./dist/ts-events.min.js
-cp -f ./node_modules/vue/dist/vue.min.js ./dist/vue.min.js
-cp -f ./node_modules/vuetify/dist/vuetify.min.js ./dist/vuetify.min.js
