f7589575185761d82d1630fb6ad86e6c307a3108
chen3 <laoc1995@163.com>
2018-09-09 09:50:39 +0800

    更新为使用类的形式

--- src/App.vue
+++ src/App.vue
@@ -13,20 +13,24 @@
             </v-list>
         </v-navigation-drawer> 
         <v-toolbar color="indigo" :dark="true" :fixed="true" :app="true">
-            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer" v-if="false"/>
             <v-toolbar-title>白名单</v-toolbar-title>
         </v-toolbar>
         <v-content>
-
+            <white-list/>
         </v-content>
     </v-app>
 </template>
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-    data: () => ({
-        drawer: null
-    })
+import Vue from "vue";
+import WhiteList from "./Components/WhiteList.vue";
+import Component from 'vue-class-component';
+
+@Component({
+    components: { WhiteList }
 })
+export default class extends Vue {
+    public drawer: boolean = false;
+}
 </script>
 
